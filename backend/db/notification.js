49372b4ad3e1c8e9201cabc86d41c64db0961066
Tertullien KOUMETIO TANE <letertullien@gmail.com>
2026-06-09 16:05:48 -0400

    Ajout champ url dans modèle Notification

--- backend/db/notification.js
+++ backend/db/notification.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 const notificationSchema = new mongoose.Schema({
   title:           { type: String, required: true },   // Titre de la notification
   body:            { type: String, required: true },   // Corps de la notification
+  url:             { type: String, default: "/" },     // URL de destination
   sentAt:          { type: Date, default: Date.now },  // Date d'envoi
   recipientsCount: { type: Number, default: 0 },       // Nombre total d'abonnés
   successCount:    { type: Number, default: 0 },       // Envois réussis
