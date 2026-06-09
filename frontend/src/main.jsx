3ca6da66531fdcacbf80a88eeafd6f2903d0d3df
Tertullien KOUMETIO TANE <letertullien@gmail.com>
2026-06-09 10:47:48 -0400

    config gitpage hashrouter dans main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,21 @@
- import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";    
 import './index.css'
 import App from './App.jsx'
 
- 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
 
 // Enregistrement du Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/avis-mtl/sw.js')   
       .then((reg) => console.log('SW enregistré :', reg.scope))
       .catch((err) => console.error('Échec enregistrement SW :', err));
   });
